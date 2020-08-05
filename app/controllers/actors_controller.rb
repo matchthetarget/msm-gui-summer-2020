dad87bafefbc5969c923b494122372594cedef3f
Jelani Woods <jelani@firstdraft.com>
2020-08-05 15:52:38 -0500

    wip

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -14,4 +14,39 @@ class ActorsController < ApplicationController
     
     render({ :template => "actor_templates/show.html.erb" })
   end
+
+
+  def create
+    the_actor = Actor.new
+    the_actor.name = params.fetch("query_name")
+    the_actor.bio = params.fetch("query_bio")
+    the_actor.dob = params.fetch("query_dob")
+    the_actor.image = params.fetch("query_image")
+    the_actor.save
+
+    redirect_to("/actors/#{the_actor.id}")
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    matching_actors = Actor.where({ :id => the_id })
+    the_actor = matching_actors.at(0)
+
+    the_actor.name = params.fetch("query_name")
+    the_actor.bio = params.fetch("query_bio")
+    the_actor.dob = params.fetch("query_dob")
+    the_actor.image = params.fetch("query_image")
+    the_actor.save
+
+    redirect_to("/actors/#{the_id}")
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+
+    matching_actors = Actor.where({ :id => the_id })
+    the_actor = matching_actors.at(0)
+    the_actor.destroy
+    redirect_to("/actors")
+  end
 end
